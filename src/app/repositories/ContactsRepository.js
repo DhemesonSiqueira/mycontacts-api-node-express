62e4e3ef707ed79720c3818773c048d030505bdf
DhemesonSiqueira <dhemeson_loran@hotmail.com>
2021-08-12 23:52:39 -0300

    initial commit

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,9 +1,16 @@
-const { uuid } = require('uuidv4');
-const contacts = [
+const { v4 : uuid } = require('uuid');
+let contacts = [
   {
     id: uuid(),
     name: 'Dhemeson Siqueira',
     email: 'dhemeson@gmail.com',
+    phone: '0800000000',
+    category_id: uuid(),
+  },
+  {
+    id: uuid(),
+    name: 'Loran Siqueira',
+    email: 'Loran@gmail.com',
     phone: '0800000000',
     category_id: uuid(),
   },
@@ -12,6 +19,19 @@ const contacts = [
 class ContactsRepository {
   findAll() {
     return new Promise((resolve, rejects) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve, rejects) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve, rejects) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
   }
 }
 
